deb07596a9c1faa1b955e1a761619af3c4356eb1
Peter Coyle <coyle.peters@gmail.com>
2017-11-15 15:23:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,33 +1,39 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
 
-// call functions here
-  function getIt() {
-    $('p').on('click', function(){
-      alert("Hey!")
-    })
-  }
-
-  function frameIt() {
-    $('img').on('load', function() {
-      var image = $('img')
-      image.className = "tasty"
-      image.frame.style.border-color = "thick solid red"
-    })
-  }
+function frameIt() {
+  $('img').on('load', function() {
+    var image = $('img')
+    image.className = "tasty"
+    image.frame.style.border-color = "thick solid red"
+  })
+}
 
 function pressIt() {
-  $('input').on('keydown', function(key) {
-    if(key === 71){
-      alert("you pressed 'g'")
-    }
-  })
+$('input').on('keydown', function(key) {
+  if(key === 71){
+    alert("you pressed 'g'")
+  }
+})
 }
 
 function submitIt() {
-  $('form').on('submit', function() {
-    alert("Your form is going to be submitted now.")
-  })
+$('form').on('submit', function() {
+  alert("Your form is going to be submitted now.")
+})
 }
+
+
+$(document).ready(function(){
+
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+
 });
